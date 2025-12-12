7511a10e20a1398b14be094f8541b00daaa2583b
Manikanda Prabhu RaviChandran <rmkp@hotmail.com>
2025-12-12 22:53:14 +0530

    fix: testing double deployment

--- app/index.js
+++ app/index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello from sample-app</h1>\n<p>update from sample app</p>');
+  res.send('<h1>Hello from sample-app</h1>\n<p>testing double deployment</p>');
 });
 
 app.listen(port, () => console.log(`sample-app listening on ${port}`));
